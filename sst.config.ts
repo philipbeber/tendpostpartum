8b6ef99c2013d9aca110719052bc67df2142244c
Phil Beber <phil@wabisabi.local>
2026-03-30 11:43:01 -0700

    prod URL

--- sst.config.ts
+++ sst.config.ts
@@ -4,13 +4,21 @@
 export default $config({
   app(input) {
     return {
-      name: "tendpostpartum",
+      name: "tendpostpartumcare",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
   async run() {
-    new sst.aws.Nextjs("MyWeb");
+    new sst.aws.Nextjs("MyWeb", {
+      domain:
+        $app.stage === "production"
+          ? {
+              name: "tendpostpartumcare.com",
+              redirects: ["www.tendpostpartumcare.com"],
+            }
+          : undefined,
+    });
   },
 });
